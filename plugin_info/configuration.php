96c20b230b2ef0a23cdc80fc48fee8dc5647e106
Joël Kwan <joel.kwan@univ-reunion.fr>
2019-06-19 00:09:24 +0400

    Génération de la base
    Utilisation d'extra-tools

---
+++ plugin_info/configuration.php
@@ -0,0 +1,54 @@
+<?php
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
+include_file('core', 'authentification', 'php');
+if (!isConnect()) {
+    include_file('desktop', '404', 'php');
+    die();
+}
+?>
+<form class="form-horizontal">
+  <fieldset>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">
+        {{Ville}}
+      </label>
+      <div class="col-sm-9">
+        <input class="configKey form-control" data-l1key="city" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">
+        {{Alerte cyclonique}}
+      </label>
+      <div class="col-sm-9">
+        <input class="configKey form-control" type="checkbox" data-l1key="cyclone" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">
+        {{Alerte vigilance}}
+      </label>
+      <div class="col-sm-9">
+        <input class="configKey form-control" type="checkbox" data-l1key="vigilance" />
+      </div>
+    </div>
+  </fieldset>
+</form>
