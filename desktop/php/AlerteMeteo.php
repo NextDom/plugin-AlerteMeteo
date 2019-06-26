dbe02dfeac7817cbdf5505b790d7cb4d137813ab
Joël Kwan <joel.kwan@univ-reunion.fr>
2019-06-26 15:20:36 +0400

    Desktop/AlerteMeteo - Test menu d'actions

--- desktop/php/AlerteMeteo.php
+++ desktop/php/AlerteMeteo.php
@@ -21,4 +21,35 @@ if (!isConnect('admin')) {
     throw new Exception('{{401 - Accès non autorisé}}');
 }
 
-echo ('Hello World');
+
+$plugin = plugin::byId('AlerteMeteo');
+
+sendVarToJS('eqType', $plugin->getId());
+$eqLogics = eqLogic::byType($plugin->getId());
+?>
+<div class="row row-overflow">
+    <!-- Menu d'actions -->
+    <div class="col-lg-2 col-md-3 col-sm-4">
+        <div class="bs-sidebar">
+            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+                <!-- Ajout -->
+                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add">
+                    <i class="fa fa-plus-circle"></i> {{Ajouter}}
+                </a>
+                <!-- Recherche -->
+                <li class="filter" style="margin-bottom: 5px;">
+                    <input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%" />
+                </li>
+                <?php
+                foreach ($eqLogics as $eqLogic) {
+                    $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+                    echo '<li class="li_eqLogic cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="' . $opacity . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+</div>
+<?php
+include_file('desktop', 'AlerteMeteo', 'js', 'AlerteMeteo');
+include_file('core', 'plugin.template', 'js');
